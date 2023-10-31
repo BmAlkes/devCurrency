23fb74d86608b119524f5e542c4906cb8408537f
bmalkes <bmalkes@gmail.com>
2023-10-31 12:03:57 +0200

    feat:add home table

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,40 @@
-import style from "./home.module.css";
+import { Link } from "react-router-dom";
+import styles from "./home.module.css";
+import { BiSearch } from "react-icons/bi";
 export const Home = () => {
-  return <div> Pagina Home</div>;
+  return (
+    <main className={styles.container}>
+      <form className={styles.form}>
+        <input placeholder=" Search the symbol of coin: BTC" />
+        <button type="submit">
+          <BiSearch size={30} color="#fff" />
+        </button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Coins </th>
+            <th scope="col">Market Value </th>
+            <th scope="col">Price </th>
+            <th scope="col">Volume </th>
+          </tr>
+        </thead>
+        <tbody id="tbody">
+          <tr className={styles.tr}>
+            <td className={styles.tdLabel}>
+              <Link to="/detail/btc">
+                <span className={styles.link}>Bitcoin</span> | BTC
+              </Link>
+            </td>
+            <td className={styles.tdLabel}>$122</td>
+            <td className={styles.tdLabel}>$122</td>
+            <td className={styles.tdProfit}>
+              <span>-5.3</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
 };
